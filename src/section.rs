935aad71727b84e94cfa42ce7f191f8f08909e93
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-01 01:34:38 +0200

    Fix logic for parser

--- src/section.rs
+++ src/section.rs
@@ -9,17 +9,28 @@ impl Section {
     pub fn parse_content(content: &str) -> Vec<Section> {
         let chars = content.chars().collect::<Vec<char>>();
         let mut sections = Vec::new();
-        let mut prefix = String::new();
-        for i in 0..chars.len() - 1 {
-            if chars[i] == '{' && chars[i + 1] == '}' {
-                sections.push(Section::editable(prefix));
-                prefix = String::new();
+        let mut prefix = Vec::new();
+        for c in chars {
+            if c != '}' {
+                prefix.push(c);
+                continue;
+            }
+
+            if let Some(c) = prefix.last() {
+                if c == &'{' {
+                    prefix.pop();
+                    sections.push(Section::editable(prefix.iter().collect()));
+                    prefix = Vec::new();
+                } else {
+                    prefix.push(*c);
+                }
             }
         }
 
         if !prefix.is_empty() {
-            sections.push(Section::tail(prefix));
+            sections.push(Section::tail(prefix.iter().collect()));
         }
+
         sections
     }
 
@@ -43,14 +54,23 @@ mod test {
     use super::Section;
 
     #[test]
-    fn return_string_as_section_prefix() {
+    fn return_string_as_section_tail() {
         let sections = Section::parse_content("Hello this is content with no value");
-        assert!(sections.is_empty());
+        assert!(sections.first().unwrap().suffix.is_none());
     }
 
     #[test]
     fn return_correct_section() {
         let sections = Section::parse_content("Hello this is content with no value {}");
         assert!(!sections.is_empty());
+    }
+
+    #[test]
+    fn parse_multiple_sections_including_tail() {
+        let sections = Section::parse_content(
+            "Hello this is content with no value {} This gonna be tail moving forward.",
+        );
+        assert!(sections.first().unwrap().suffix.is_some());
+        assert!(sections.get(1).unwrap().suffix.is_none());
     }
 }
