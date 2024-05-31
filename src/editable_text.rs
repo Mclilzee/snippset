4af0626f6cc913976a00c5ad0bf0eae538417522
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-05-31 20:08:36 +0200

    Work with indexes in editable text

--- src/editable_text.rs
+++ src/editable_text.rs
@@ -1,16 +1,14 @@
 use crossterm::event::KeyCode;
 
 pub struct EditableText {
-    pub column: u16,
-    pub row: u16,
+    index: usize,
     chars: Vec<char>,
 }
 
 impl EditableText {
-    pub fn new(column: u16, row: u16) -> Self {
+    pub fn new() -> Self {
         EditableText {
-            column,
-            row,
+            index: 0,
             chars: Vec::new(),
         }
     }
@@ -19,33 +17,19 @@ impl EditableText {
         match code {
             KeyCode::Char(c) => {
                 self.chars.push(c);
-                self.column += 1;
+                self.index += 1;
             }
             KeyCode::Enter => {
                 self.chars.push('\r');
-                self.column = 0;
-                self.row += 1;
+                self.index += 1;
             }
             KeyCode::Backspace => {
                 if !self.chars.is_empty() {
-                    let removed = self.chars.remove(self.column as usize);
-                    self.column -= 1;
-                    if removed == '\r' {
-                        self.row -= 1;
-                    };
+                    self.chars.remove(self.index);
+                    self.index -= 1;
                 }
             }
             _ => (),
         };
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::EditableText;
-
-    #[test]
-    fn advances_column() {
-        let mut editable = EditableText::new(0, 0);
     }
 }
