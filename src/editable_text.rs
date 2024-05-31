03cc3bcff6f219371b6d663b250f8a387920f98f
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-05-31 21:25:01 +0200

    Refactor

--- src/editable_text.rs
+++ src/editable_text.rs
@@ -28,6 +28,10 @@ impl EditableText {
     pub fn text(&self) -> String {
         self.chars.iter().collect()
     }
+
+    pub fn cursor_left(&mut self) {
+        self.cursor -= 1;
+    }
 }
 
 #[cfg(test)]
@@ -36,44 +40,44 @@ mod test {
 
     #[test]
     fn advances_column() {
-        let editable = EditableText::new();
+        let editable = create_editable("");
         assert!(editable.text().is_empty());
     }
 
     #[test]
     fn insert_chars_correctly() {
-        let mut editable = EditableText::new();
-        editable.insert('h');
-        editable.insert('e');
-        editable.insert('l');
-        editable.insert('l');
-        editable.insert('o');
-
+        let editable = create_editable("hello");
         assert_eq!(5, editable.chars.len());
     }
 
     #[test]
     fn return_correct_text() {
-        let mut editable = EditableText::new();
-        editable.insert('h');
-        editable.insert('e');
-        editable.insert('l');
-        editable.insert('l');
-        editable.insert('o');
-
+        let editable = create_editable("hello");
         assert_eq!("hello".to_owned(), editable.text());
     }
 
     #[test]
     fn removes_currectly() {
-        let mut editable = EditableText::new();
-        editable.insert('h');
-        editable.insert('e');
-        editable.insert('l');
-        editable.insert('l');
-        editable.insert('o');
+        let mut editable = create_editable("hello");
         editable.delete();
 
         assert_eq!("hell".to_owned(), editable.text());
+    }
+
+    #[test]
+    fn moves_cursor_left() {
+        let mut editable = create_editable("hello");
+        editable.cursor_left();
+        editable.delete();
+
+        assert_eq!("helo".to_owned(), editable.text());
+    }
+
+    fn moves_cursor_right() {}
+
+    fn create_editable(text: &str) -> EditableText {
+        let mut editable = EditableText::new();
+        text.chars().for_each(|c| editable.insert(c));
+        editable
     }
 }
