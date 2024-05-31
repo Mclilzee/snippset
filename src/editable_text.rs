630e9fab8a2021d836c1a74838a665368736f1cd
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-05-31 21:18:05 +0200

    Fix indexing text

--- src/editable_text.rs
+++ src/editable_text.rs
@@ -1,26 +1,26 @@
 pub struct EditableText {
-    index: usize,
+    cursor: usize,
     chars: Vec<char>,
 }
 
 impl EditableText {
     pub fn new() -> Self {
         EditableText {
-            index: 0,
+            cursor: 0,
             chars: Vec::new(),
         }
     }
 
     pub fn insert(&mut self, c: char) {
         self.chars.push(c);
-        self.index += 1;
+        self.cursor += 1;
     }
 
-    pub fn remove(&mut self) {
+    pub fn delete(&mut self) {
         if !self.chars.is_empty() {
-            self.chars.remove(self.index);
-            if self.index > 0 {
-                self.index -= 1;
+            self.chars.remove(self.cursor - 1);
+            if self.cursor > 0 {
+                self.cursor -= 1;
             }
         }
     }
@@ -38,5 +38,42 @@ mod test {
     fn advances_column() {
         let editable = EditableText::new();
         assert!(editable.text().is_empty());
+    }
+
+    #[test]
+    fn insert_chars_correctly() {
+        let mut editable = EditableText::new();
+        editable.insert('h');
+        editable.insert('e');
+        editable.insert('l');
+        editable.insert('l');
+        editable.insert('o');
+
+        assert_eq!(5, editable.chars.len());
+    }
+
+    #[test]
+    fn return_correct_text() {
+        let mut editable = EditableText::new();
+        editable.insert('h');
+        editable.insert('e');
+        editable.insert('l');
+        editable.insert('l');
+        editable.insert('o');
+
+        assert_eq!("hello".to_owned(), editable.text());
+    }
+
+    #[test]
+    fn removes_currectly() {
+        let mut editable = EditableText::new();
+        editable.insert('h');
+        editable.insert('e');
+        editable.insert('l');
+        editable.insert('l');
+        editable.insert('o');
+        editable.delete();
+
+        assert_eq!("hell".to_owned(), editable.text());
     }
 }
