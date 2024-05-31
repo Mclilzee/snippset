45963eb1fcfc355e79ad8a4d657977fb294d8ac1
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-05-31 15:18:23 +0200

    Print title and snippet

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
-use std::io;
+use std::collections::HashMap;
+use std::io::{self, stdout, Stdout, Write};
 
+use crossterm::cursor;
 use crossterm::event::{KeyEventKind, KeyModifiers};
+use crossterm::style::Print;
 use crossterm::{
     event::{read, Event, KeyCode},
     terminal::{disable_raw_mode, enable_raw_mode},
 };
+use crossterm::{execute, terminal};
 use inquire::{InquireError, Select};
 
 fn main() -> Result<(), InquireError> {
-    Select::new("Choose snippet", snippets_list()).prompt()?;
+    execute!(
+        stdout(),
+        terminal::Clear(terminal::ClearType::All),
+        cursor::EnableBlinking,
+        cursor::MoveTo(0, 0)
+    )?;
+    let mut map = HashMap::new();
+    map.insert(
+        "hello snippet",
+        "This hello is a snippet {} placeholder {} lets go? {}",
+    );
+
+    let key = Select::new("Choose snippet", map.keys().collect()).prompt()?;
+    let snippet = map.get(key).unwrap();
 
     enable_raw_mode()?;
-    if let Err(e) = print_events() {
+    if let Err(e) = handle_snippet(key, snippet) {
         println!("Error: {:?}\r", e);
     }
 
@@ -19,21 +36,41 @@ fn main() -> Result<(), InquireError> {
     Ok(())
 }
 
-fn snippets_list() -> Vec<String> {
-    vec!["First snippet".into(), "Other snippet".into()]
-}
+fn handle_snippet(title: &str, snippet: &str) -> io::Result<()> {
+    let mut stdout = stdout();
+    execute!(
+        stdout,
+        cursor::MoveTo(0, 0),
+        terminal::Clear(terminal::ClearType::FromCursorDown),
+        Print(format!("Snippet: {title}\r")),
+        cursor::MoveDown(1),
+        Print("--------------------------------------\r"),
+        cursor::MoveDown(1),
+    )?;
 
-fn print_events() -> io::Result<()> {
     loop {
+        execute!(
+            stdout,
+            cursor::MoveTo(0, 7),
+            terminal::Clear(terminal::ClearType::FromCursorDown),
+            Print(snippet),
+            cursor::MoveTo(0, 2),
+            Print("text")
+        )?;
+
         if let Event::Key(event) = read()? {
-            if event.kind == KeyEventKind::Press && event.code == KeyCode::Esc {
-                break;
+            if event.kind != KeyEventKind::Press {
+                continue;
             }
 
-            if event.kind == KeyEventKind::Press {
-                println!("{event:?}");
+            if event.code == KeyCode::Esc
+                || (event.modifiers == KeyModifiers::CONTROL && event.code == KeyCode::Char('c'))
+            {
+                break;
             }
         }
     }
+
+    execute!(stdout, cursor::DisableBlinking)?;
     Ok(())
 }
