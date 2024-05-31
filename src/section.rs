5ba83a1f32a729c9ccd72255d2cee495e3359f86
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-05-31 21:13:18 +0200

    Refactor

--- src/section.rs
+++ src/section.rs
@@ -8,18 +8,12 @@ pub struct Section {
 impl Section {
     pub fn parse_content(content: &str) -> Vec<Section> {
         let chars = content.chars().collect::<Vec<char>>();
-        let mut column = 0;
-        let mut row = 0;
         let mut sections = Vec::new();
         let mut prefix = String::new();
         for i in 0..chars.len() - 1 {
-            column += 1;
             if chars[i] == '{' && chars[i + 1] == '}' {
                 sections.push(Section::editable(prefix));
                 prefix = String::new();
-            } else if chars[i] == '\r' {
-                row += 1;
-                column = 0;
             }
         }
 
