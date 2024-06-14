b68dc0c69be5700d1b6e48219978bf794c325a6f
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-14 12:08:35 +0200

    Fix error in section

--- src/sections/section.rs
+++ src/sections/section.rs
@@ -15,30 +15,6 @@ impl Section {
         Self::StaticText(StaticText::new(chars))
     }
 
-    pub fn range(&self, width: u16) -> TextRange {
-        let mut row = 0;
-        let mut column = 0;
-        let mut text = String::with_capacity(self.chars().len());
-        let mut cursor_position = None;
-        for (i, c) in self.chars().iter().enumerate() {
-            text.push(*c);
-            if c == &'\r' || column > width {
-                row += 1;
-                column = 0;
-            } else {
-                column += 1;
-            }
-
-            if let Self::Editable(ed) = self {
-                if ed.cursor == i {
-                    cursor_position = Some((column, row));
-                }
-            }
-        }
-
-        TextRange::new((column, row), text, cursor_position)
-    }
-
     fn chars(&self) -> &Vec<char> {
         match self {
             Self::Editable(ed) => &ed.chars,
@@ -46,27 +22,33 @@ impl Section {
         }
     }
 }
-//
-// #[cfg(test)]
-// mod test {
-//     use super::Section;
-//
-//     #[test]
-//     fn get_full_text_tail() {
-//         let section = Section::static_text("Hello this".to_string());
-//         assert_eq!(section.text(), "Hello this".to_owned());
-//     }
-//
-//     #[test]
-//     fn get_full_text_editable() {
-//         let mut section = Section::editable();
-//         fill_editable(&mut section, "stuff goes here");
-//         assert_eq!(section.text(), "stuff goes here".to_owned());
-//     }
-//
-//     fn fill_editable(section: &mut Section, string: &str) {
-//         if let Section::Editable(ed) = section {
-//             string.chars().for_each(|c| ed.insert(c));
-//         }
-//     }
-// }
+
+#[cfg(test)]
+mod test {
+    use super::Section;
+
+    #[test]
+    fn get_static_text() {
+        let section = Section::static_text("Hello this".chars().collect());
+        assert_eq!(
+            section.chars(),
+            &"Hello this".chars().collect::<Vec<char>>()
+        );
+    }
+
+    #[test]
+    fn get_editable_text() {
+        let mut section = Section::editable();
+        match section {
+            Section::Editable(ref mut txt) => {
+                "This is editable".chars().for_each(|c| txt.insert(c))
+            }
+            _ => {}
+        }
+
+        assert_eq!(
+            section.chars(),
+            &"This is editable".chars().collect::<Vec<char>>()
+        );
+    }
+}
