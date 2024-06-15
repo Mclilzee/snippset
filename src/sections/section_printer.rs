0cd8ca66a45394fc0935687519d7e681ab2f4b6f
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-16 01:13:24 +0200

    Get printer to print snippet

--- src/sections/section_printer.rs
+++ src/sections/section_printer.rs
@@ -1,6 +1,6 @@
-use std::io::{self, stdout, Stdout};
-
+use crossterm::QueueableCommand;
 use crossterm::{cursor, execute, style::Print, terminal};
+use std::io::{self, stdout, Stdout};
 
 use crate::sections::section::Section;
 
@@ -8,11 +8,15 @@ const TITLE_PADDING: u16 = 5;
 
 pub struct SectionPrinter {
     stdout: Stdout,
+    snippet_start: (u16, u16),
 }
 
 impl SectionPrinter {
     pub fn new() -> Self {
-        Self { stdout: stdout() }
+        Self {
+            stdout: stdout(),
+            snippet_start: (0, 0),
+        }
     }
 
     pub fn print_snippet(&self, sections: &[Section]) -> io::Result<()> {
@@ -27,14 +31,34 @@ impl SectionPrinter {
     }
 
     pub fn print_title(&mut self, title: &str) -> io::Result<()> {
-        execute!(
-            stdout(),
-            cursor::MoveTo(0, 0),
-            terminal::Clear(terminal::ClearType::FromCursorDown),
-            Print(format!("Snippet: {}\r", title)),
-            cursor::MoveDown(1),
-            Print("--------------------------------------\r"),
-        )?;
+        let (width, _) = terminal::size()?;
+        self.stdout
+            .queue(cursor::MoveTo(0, 0))?
+            .queue(terminal::Clear(terminal::ClearType::FromCursorDown))?
+            .queue(Print("Snippet: "))?;
+
+        let mut column = 0;
+        let mut row = 0;
+
+        for c in title.chars() {
+            if c == '\n' {
+                row += 1;
+                column = 0;
+            } else {
+                column += 1;
+            }
+
+            if column > width {
+                row += 1;
+                column = 0;
+            }
+            self.stdout.queue(Print(c));
+        }
+
+        self.stdout
+            .queue(Print("\r"))?
+            .queue(cursor::MoveDown(1))?
+            .queue(Print("--------------------------------------\r"))?;
 
         Ok(())
     }
