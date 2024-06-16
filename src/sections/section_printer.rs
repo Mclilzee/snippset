60e719e12e9d5b4c6aaee845f7ac6743cbd59c7c
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-16 02:24:25 +0200

    Add printing the title

--- src/sections/section_printer.rs
+++ src/sections/section_printer.rs
@@ -5,45 +5,39 @@ use std::io::{self, stdout, Stdout, Write};
 use crate::sections::section::Section;
 
 const TITLE_PADDING: u16 = 2;
-const LINE_POSITION_UNDER_HEADER: u16 = 1;
 
 pub struct SectionPrinter {
     stdout: Stdout,
-    body_row: u16,
-    width: u16,
 }
 
 impl SectionPrinter {
     pub fn new() -> Self {
-        Self {
-            stdout: stdout(),
-            body_row: 0,
-            width: 0,
-        }
+        Self { stdout: stdout() }
     }
 
     pub fn print_header(&mut self, title: &str) -> io::Result<()> {
         let (width, _) = terminal::size()?;
-
-        self.stdout
-            .queue(cursor::MoveTo(0, 0))?
-            .queue(terminal::Clear(terminal::ClearType::FromCursorDown))?
-            .queue(Print(format!("Snippet: {title}\n")))?
-            .queue(cursor::MoveDown(2))?
-            .queue(cursor::MoveToColumn(0))?
-            .queue(Print((0..width).map(|_| '-').collect::<String>()))?
-            .queue(cursor::MoveDown(1))?
-            .queue(cursor::MoveToColumn(0))?
-            .queue(cursor::SavePosition)?;
-
-        self.stdout.flush()?;
+        execute!(
+            self.stdout,
+            cursor::MoveTo(0, 0),
+            terminal::Clear(terminal::ClearType::FromCursorDown),
+            Print(format!("Snippet: {title}")),
+            cursor::MoveDown(1),
+            cursor::MoveToColumn(0),
+            Print((0..width).map(|_| '-').collect::<String>()),
+            cursor::MoveDown(TITLE_PADDING),
+            cursor::MoveToColumn(0),
+            cursor::SavePosition
+        )?;
         Ok(())
     }
 
     pub fn print_body(&mut self, sections: &[Section], cursor_index: usize) -> io::Result<()> {
         for section in sections.iter() {
             for (i, c) in section.prefix.iter().enumerate() {
-                self.stdout.queue(Print(c))?;
+                self.stdout
+                    .queue(cursor::RestorePosition)?
+                    .queue(Print(c))?;
             }
 
             let suffix = match section.suffix.as_ref() {
