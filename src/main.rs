9701d0dfa56f5a9c7864a0509bd3d852c052face
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-17 11:49:18 +0200

    Add ability to import file with serde

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,23 @@ mod sections;
 use args::Args;
 use clap::Parser;
 use sections::snippet_engine::SnippetEngine;
-use std::collections::HashMap;
-use std::path::PathBuf;
-use std::process::exit;
+use std::{collections::HashMap, fs::File, io::BufReader};
 
 use inquire::{InquireError, Select};
 
 fn main() -> Result<(), InquireError> {
     let config = Args::parse();
+    let file = File::open(config.file).unwrap();
+    let reader = BufReader::new(file);
+    let map: HashMap<String, String> = serde_json::from_reader(reader).unwrap();
 
-    // let key = Select::new("Choose snippet", map.keys().collect()).prompt()?;
-    // let snippet = map.get(key).unwrap();
-    // let mut snippet_engine = SnippetEngine::new(key, snippet);
+    let key = Select::new("Choose snippet", map.keys().collect()).prompt()?;
+    let snippet = map.get(key).unwrap();
+    let mut snippet_engine = SnippetEngine::new(key, snippet);
 
-    // if let Err(e) = snippet_engine.start() {
-    //     println!("Error: {:?}\r", e);
-    // }
+    if let Err(e) = snippet_engine.start() {
+        println!("Error: {:?}\r", e);
+    }
 
     Ok(())
 }
