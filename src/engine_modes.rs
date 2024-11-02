654c4e8a8a69d5420350f53b0646ffcd256496ea
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-11-02 20:25:31 +0100

    Update structure of code

---
+++ src/engine_modes.rs
@@ -0,0 +1,63 @@
+use crate::sections::snippet_engine::SnippetEngine;
+use crate::Snippets;
+use inquire::{InquireError, Select, Text};
+use std::fmt::Display;
+use std::path::PathBuf;
+use std::process::exit;
+use std::{collections::HashMap, fs::File, io::BufReader};
+
+pub fn start_editing_engine(path: PathBuf) -> Result<(), InquireError> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let map: Snippets = serde_json::from_reader(reader).unwrap_or_else(print_error);
+    let key = Select::new("Choose snippet", map.keys().collect()).prompt()?;
+    let snippet = map.get(key).unwrap();
+
+    let mut snippet_engine = SnippetEngine::new(key, snippet)?;
+    let result = snippet_engine.start()?;
+    print!("{esc}[2J{esc}[1;1H{result}", esc = 27 as char);
+
+    Ok(())
+}
+
+pub fn add_to_file(path: PathBuf) -> Result<(), InquireError> {
+    let mut map: Snippets = match File::open(&path) {
+        Ok(f) => serde_json::from_reader(BufReader::new(f)).unwrap_or_else(print_error),
+        Err(_) => HashMap::new(),
+    };
+
+    let title = Text::new("Title: ").prompt()?;
+    let snippet = Text::new("Snippet: ").prompt()?.replace("\\n", "\n");
+
+    map.insert(title, snippet);
+    serde_json::to_writer(File::create(&path)?, &map).unwrap_or_else(print_error);
+    exit(0);
+}
+
+pub fn edit_file(path: PathBuf) -> Result<(), InquireError> {
+    let file = File::open(&path)?;
+    let reader = BufReader::new(file);
+    let mut map: Snippets = serde_json::from_reader(reader).unwrap_or_else(print_error);
+
+    let key = Select::new("Choose snippet to edit", map.keys().collect())
+        .prompt()?
+        .to_owned();
+
+    let snippet = map.get(&key).unwrap();
+
+    let title = Text::new("Title: ").with_initial_value(&key).prompt()?;
+    let snippet = Text::new("Snippet: ")
+        .with_initial_value(snippet)
+        .prompt()?
+        .replace("\\n", "\n");
+
+    map.remove(&key);
+    map.insert(title, snippet);
+    serde_json::to_writer(File::create(&path)?, &map).unwrap_or_else(print_error);
+    exit(0);
+}
+
+fn print_error<E: Display, T>(e: E) -> T {
+    eprintln!("{e}");
+    exit(1);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
 mod args;
+mod engine_modes;
 mod sections;
+
+use std::{collections::HashMap, io::stdout};
 
 use args::Args;
 use clap::Parser;
 use crossterm::{
-    execute,
-    cursor,
+    cursor, execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen},
 };
+use engine_modes::*;
 
-use inquire::{InquireError, Select, Text};
-use sections::snippet_engine::SnippetEngine;
-use std::fmt::Display;
-use std::path::PathBuf;
-use std::process::exit;
-use std::{
-    collections::HashMap,
-    fs::File,
-    io::{stdout, BufReader},
-};
+use inquire::InquireError;
 
 type Snippets = HashMap<String, String>;
 
@@ -38,60 +32,4 @@ fn main() -> Result<(), InquireError> {
     execute!(stdout(), LeaveAlternateScreen)?;
 
     result
-}
-
-fn start_editing_engine(path: PathBuf) -> Result<(), InquireError> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    let map: Snippets = serde_json::from_reader(reader).unwrap_or_else(print_error);
-    let key = Select::new("Choose snippet", map.keys().collect()).prompt()?;
-    let snippet = map.get(key).unwrap();
-
-    let mut snippet_engine = SnippetEngine::new(key, snippet)?;
-    let result = snippet_engine.start()?;
-    print!("{esc}[2J{esc}[1;1H{result}", esc = 27 as char);
-
-    Ok(())
-}
-
-fn add_to_file(path: PathBuf) -> Result<(), InquireError> {
-    let mut map: Snippets = match File::open(&path) {
-        Ok(f) => serde_json::from_reader(BufReader::new(f)).unwrap_or_else(print_error),
-        Err(_) => HashMap::new(),
-    };
-
-    let title = Text::new("Title: ").prompt()?;
-    let snippet = Text::new("Snippet: ").prompt()?.replace("\\n", "\n");
-
-    map.insert(title, snippet);
-    serde_json::to_writer(File::create(&path)?, &map).unwrap_or_else(print_error);
-    exit(0);
-}
-
-fn edit_file(path: PathBuf) -> Result<(), InquireError> {
-    let file = File::open(&path)?;
-    let reader = BufReader::new(file);
-    let mut map: Snippets = serde_json::from_reader(reader).unwrap_or_else(print_error);
-
-    let key = Select::new("Choose snippet to edit", map.keys().collect())
-        .prompt()?
-        .to_owned();
-
-    let snippet = map.get(&key).unwrap();
-
-    let title = Text::new("Title: ").with_initial_value(&key).prompt()?;
-    let snippet = Text::new("Snippet: ")
-        .with_initial_value(snippet)
-        .prompt()?
-        .replace("\\n", "\n");
-
-    map.remove(&key);
-    map.insert(title, snippet);
-    serde_json::to_writer(File::create(&path)?, &map).unwrap_or_else(print_error);
-    exit(0);
-}
-
-fn print_error<E: Display, T>(e: E) -> T {
-    eprintln!("{e}");
-    exit(1);
 }
